fe2d18509dc4eb8eacb7862aac64ca2a10d12c41
CodeSandbox <support@codesandbox.io>
2023-10-31 15:31:02 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-var http = require("http");
+const { createServer } = require("http");
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+async function getTodosHtml() {
+  await new Promise((res) => setTimeout(res, 2000));
+  return `
+    <ol>
+      <li>Write a blog post</li>
+      <li>Drink water</li>
+    </ol>
+  `;
+}
+
+createServer(async (req, res) => {
+  const todosHtmlPromise = getTodosHtml();
+  res.write(`
+    <html>
+      <head><title>Todos</title></head>
+      <body>
+        <h1>My Todos:</h1>
+        <div id="loading-todos">loading...</div>
+        <div>
+        <button onclick="window.location.reload()" type="button">reload page</button>
+      </body>
+    </html>
+  `);
+
+  const todosHtml = await todosHtmlPromise;
+
+  res.write(`
+    <template id="for-loading-todos">${todosHtml}</template>
+    <script>
+    (() => {
+      const template = document.querySelector("#for-loading-todos");
+      document.querySelector("#loading-todos").outerHTML = template.innerHTML;
+      template.remove();
+    })()
+    </script>
+  `);
+
+  res.end();
+}).listen(8080);
